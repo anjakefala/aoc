52b90d0a553b973ebad4a3c80ad7ca9fa10b7db3
anjakefala <anja.kefala@gmail.com>
2018-12-02 11:02:06 -0500

    [2018 day 2] solved part 2

--- 2018-02.py
+++ 2018-02.py
@@ -19,8 +19,29 @@ def count_letters(words):
             box_counts[3] += 1
     return box_counts
 
+def compare(word_a, word_b):
+    for i in range(len(word_a)):
+        subset_a = word_a[:i] + word_a[i+1:]
+        subset_b = word_b[:i] + word_b[i+1:]
+        if subset_a == subset_b:
+            return (True, i)
+    return (False, -1)
+
+def find_fabric(boxes):
+    for i in range(len(boxes)):
+        current_box = boxes[i]
+        for j in range(i+1, len(boxes)):
+            second_box = boxes[j]
+            have_fabric, k = compare(current_box, second_box)
+            if have_fabric:
+                return (i, j, k)
+
+
 def checksum(box_counts):
     return box_counts[2] * box_counts[3]
 
 
 print(checksum(count_letters(read_puzzle_input())))
+boxes = read_puzzle_input()
+i, j, k = find_fabric(boxes)
+print(boxes[i][:k] + boxes[i][k+1:])
